50cb4f566ee6225b3f20b6e47551a4ae7fdd753d
bbsmithy <bsmith@cartrawler.com>
2019-06-29 14:00:40 +0100

    created rust project

---
+++ target/debug/deps/server-8815d4328330e57e.d
@@ -0,0 +1,5 @@
+/Users/briansmith/developer/tigum-api/server/target/debug/deps/server-8815d4328330e57e: src/main.rs
+
+/Users/briansmith/developer/tigum-api/server/target/debug/deps/server-8815d4328330e57e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/server.d
@@ -0,0 +1,1 @@
+/Users/briansmith/developer/tigum-api/server/target/debug/server.dSYM: /Users/briansmith/developer/tigum-api/server/src/main.rs
