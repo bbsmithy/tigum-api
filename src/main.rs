a199dc370f6af0cc05d75831fe9e7c31e5bf6b0c
bbsmithy <bsmith@cartrawler.com>
2019-06-29 15:39:48 +0100

    Set up http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let body = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!("HTTP/1.2 200 OK\r\n\r\n{}", body);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
