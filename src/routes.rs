32592132b8ffe95217a00efb74cfabdb8f317dcb
bbsmithy <bsmith@cartrawler.com>
2019-07-13 14:00:49 +0100

    Added delete and patch to routes

--- src/routes.rs
+++ src/routes.rs
@@ -2,6 +2,11 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::fs;
 
+pub fn ok_response(body:String) -> String {
+    return format!("HTTP/1.1 200 OK\r\n\r\n{}", body);
+}
+
+
 pub fn write_to_stream(stream: &mut TcpStream, response: &String){
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
@@ -10,12 +15,24 @@ pub fn write_to_stream(stream: &mut TcpStream, response: &String){
 
 pub fn handle_get_request(mut stream: TcpStream){
     let body = fs::read_to_string("get.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", body);
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", &body);
     write_to_stream(&mut stream, &response)
 }
 
 pub fn handle_post_request(mut stream: TcpStream){
     let body = fs::read_to_string("post.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", body);
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", &body);
+    write_to_stream(&mut stream, &response)
+}
+
+pub fn handle_patch_request(mut stream: TcpStream){
+    let body = fs::read_to_string("patch.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", &body);
+    write_to_stream(&mut stream, &response)
+}
+
+pub fn handle_delete_request(mut stream: TcpStream){
+    let body = fs::read_to_string("delete.html").unwrap();
+    let response = ok_response(body);
     write_to_stream(&mut stream, &response)
 }
