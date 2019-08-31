62f1ba1dec94e818c97d3492958d35b9958c5d1a
bbsmithy <bsmith@cartrawler.com>
2019-08-31 21:40:56 +0100

    Delete and Update notes routes/querys

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -14,6 +14,17 @@ pub struct TigumPgConn(databases::postgres::Connection);
 //// NOTE DB QUERYS ////
 ////////////////////////
 
+
+pub fn delete_note(conn: &TigumPgConn, note_id: i32) -> String {
+    let result = conn.execute("DELETE FROM notes WHERE id = $1", &[&note_id]).unwrap();
+    format!("{} rows deleted", result)
+}
+
+pub fn update_note(conn: &TigumPgConn, note_id: i32, note: Json<Note>) -> Json<Note> {
+    conn.execute("UPDATE notes SET title = ($2), note_content = ($3) WHERE id = ($1)", &[&note_id, &note.title, &note.note_content]).unwrap();
+    get_note(&conn, note_id)
+}
+
 pub fn get_notes(conn: &TigumPgConn, note_ids: Json<NoteIds>) -> Json<Vec<Note>> {
     let query_result = conn.query("SELECT * FROM notes WHERE id = ANY($1)", &[&note_ids.ids]).unwrap();
     let mut results: Vec<Note> = vec![];
@@ -21,14 +32,14 @@ pub fn get_notes(conn: &TigumPgConn, note_ids: Json<NoteIds>) -> Json<Vec<Note>>
         let note = Note::new(row.get(1), row.get(0), row.get(2));
         results.push(note);
     }
-    return Json(results);
+    Json(results)
 }
 
 pub fn get_note(conn: &TigumPgConn, note_id: i32) -> Json<Note> {
     let query_result = conn.query("SELECT * FROM notes WHERE id = $1", &[&note_id]).unwrap();
     let note = query_result.get(0);
     let note_response = Note::new(note.get(1), note.get(0), note.get(2));
-    return Json(note_response)
+    Json(note_response)
 }
 
 pub fn create_note(conn: &TigumPgConn, note: Json<Note>) -> String {
@@ -43,15 +54,13 @@ pub fn create_note(conn: &TigumPgConn, note: Json<Note>) -> String {
 
 pub fn delete_topic(conn: &TigumPgConn, topic_id: i32) -> String {
     let result = conn.execute("DELETE FROM topics WHERE id = $1", &[&topic_id]).unwrap();
-    let response = format!("{} rows deleted", result);
-    return response
+    format!("{} rows deleted", result)
 }
 
 
 pub fn update_topic(conn: &TigumPgConn, topic_id: i32, topic: Json<Topic>) -> Json<Topic> {
     conn.execute("UPDATE topics SET title = ($2), date_created = ($3) WHERE id = ($1)", &[&topic_id, &topic.title, &topic.date_created]).unwrap();
-    let result = get_topic(&conn, topic_id);
-    return result
+    get_topic(&conn, topic_id)
 }
 
 pub fn get_topics(conn: &TigumPgConn, topic_ids: Json<TopicIds>) -> Json<Vec<Topic>> {
@@ -61,72 +70,18 @@ pub fn get_topics(conn: &TigumPgConn, topic_ids: Json<TopicIds>) -> Json<Vec<Top
         let topic = Topic::new(row.get(1), row.get(2), row.get(0));
         results.push(topic);
     }
-    return Json(results);
+    Json(results)
 }
 
 pub fn get_topic(conn: &TigumPgConn, topic_id: i32) -> Json<Topic> {
     let query_result = conn.query("SELECT * FROM topics WHERE id = $1", &[&topic_id]).unwrap();
     let topic = query_result.get(0);
     let result = Topic::new(topic.get(1), topic.get(2), topic.get(0));
-    return Json(result)
+    Json(result)
 }
 
 pub fn create_topic(conn: &TigumPgConn, topic: Json<Topic>) -> Json<Topic> {
     conn.execute("INSERT INTO topics (id, title, date_created) VALUES ($1, $2, $3)",
                  &[&topic.topic_id, &topic.title, &topic.date_created]).unwrap();
-    let results = get_topic(&conn, topic.topic_id);
-    return results;
-}
-
-// END OF TOPIC DB QUERYS
-
-
-fn generate_test_resources(amount: i32) -> Vec<i32> {
-    let mut resources: Vec<i32> = vec![];
-    for n in 1..amount {
-        resources.push(n);
-    }
-    return resources;
-}
-
-pub fn generate_test_notes(amount: i32) -> Vec<Note> {
-    let mut notes: Vec<Note> = vec![];
-
-    for n in 1..amount {
-        let note_title = format!("Note Title Test {}", &n.to_string());
-        let note_content = generate_test_resources(10);
-        let new_note = Note::new(note_title, n, note_content);
-        notes.push(new_note);
-    }
-
-    return notes;
-}
-
-pub fn generate_single_note() -> Note {
-    let resources = generate_test_resources(10);
-    let note = Note::new(String::from("Test Single note"), 1234, resources);
-
-    note
-}
-
-pub fn generate_test_topics(amount: i32) -> Json<Vec<Topic>> {
-    let mut topics: Vec<Topic> = vec![];
-
-    for n in 1..amount {
-        let note_title = format!("Topic Title Test {}", &amount.to_string());
-        let date = "12th Test 2019".to_string();
-        let new_topic = Topic::new(note_title, date, n);
-        topics.push(new_topic);
-    }
-
-    return Json(topics);
-}
-
-pub fn generate_single_topic(topic_id: i32) -> Topic {
-    let title = "Test Single Topic".to_string();
-    let date_created = "12th of Never".to_string();
-
-    let topic = Topic::new(title, date_created, topic_id);
-
-    topic
+    get_topic(&conn, topic.topic_id)
 }
--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use db::models::topic::note::{Note, NoteId, NoteIds};
 use db::models::topic::{Topic, TopicIds};
 use db::TigumPgConn;
 use db::{create_topic, get_topic, get_topics, update_topic, delete_topic};
-use db::{create_note, get_note, get_notes};
+use db::{create_note, get_note, get_notes, update_note, delete_note};
 
 // Request Gaurds
 use guards::User;
@@ -30,6 +30,18 @@ use guards::User;
 //// NOTE ROUTES ////
 /////////////////////
 
+#[delete("/notes/<note_id>")]
+fn delete_single_note(conn: TigumPgConn, note_id: i32) -> String {
+    delete_note(&conn, note_id)
+}
+
+
+#[put("/notes/<note_id>", format = "application/json", data = "<note>")]
+fn update_single_note(conn: TigumPgConn, note_id: i32, note: Json<Note>) -> Json<Note> {
+    update_note(&conn, note_id, note)
+}
+
+
 #[post("/notes/create-note", format = "application/json", data = "<note>")]
 fn create_single_note(conn: TigumPgConn, note: Json<Note>) -> String {
     let update = create_note(&conn, note);
@@ -38,14 +50,12 @@ fn create_single_note(conn: TigumPgConn, note: Json<Note>) -> String {
 
 #[get("/notes/<note_id>")]
 fn single_note(conn: TigumPgConn, note_id: i32, _auth_user: User) -> Json<Note> {
-    let note_response = get_note(&conn, note_id);
-    note_response
+    get_note(&conn, note_id)
 }
 
 #[post("/notes", format = "application/json", data = "<note_ids>")]
 fn notes(conn: TigumPgConn, note_ids: Json<NoteIds>, _auth_user: User) -> Json<Vec<Note>> { 
-    let notes_response = get_notes(&conn, note_ids);
-    return notes_response
+    get_notes(&conn, note_ids)
 }
 
 
@@ -55,34 +65,29 @@ fn notes(conn: TigumPgConn, note_ids: Json<NoteIds>, _auth_user: User) -> Json<V
 
 #[delete("/topics/<topic_id>")]
 fn delete_single_topic(conn: TigumPgConn, topic_id: i32) -> String {
-    let result = delete_topic(&conn, topic_id);
-    return result;
+    delete_topic(&conn, topic_id)
 }
 
 
 #[put("/topics/<topic_id>", format = "application/json", data = "<topic>")]
 fn update_single_topic(conn: TigumPgConn, topic_id: i32, topic: Json<Topic>, auth_user: User) -> Json<Topic> {
-    let result = update_topic(&conn, topic_id, topic);
-    return result;
+    update_topic(&conn, topic_id, topic)
 }
 
 
 #[post("/topics/create-topic", format = "application/json", data = "<topic>")]
 fn create_single_topic(conn: TigumPgConn, topic: Json<Topic>, auth_user: User) -> Json<Topic> {
-    let update = create_topic(&conn, topic);
-    update
+    create_topic(&conn, topic)
 }
 
 #[get("/topics/<topic_id>")]
 fn single_topic(conn: TigumPgConn, topic_id: i32, _auth_user: User) -> Json<Topic> {
-    let topic = get_topic(&conn, topic_id);
-    return topic;
+    get_topic(&conn, topic_id)
 }
 
 #[post("/topics", format = "application/json", data = "<topic_ids>")]
 fn topics(conn: TigumPgConn, topic_ids: Json<TopicIds>, _auth_user: User) -> Json<Vec<Topic>> {
-    let result_topic = get_topics(&conn, topic_ids);
-    return result_topic;
+    get_topics(&conn, topic_ids)
 }
 
 // CORS Prelight Request Handler
@@ -93,9 +98,8 @@ fn preflight_handler() {
 }
 
 fn main() {
-
     rocket::ignite()
-        .mount("/", routes![topics, single_topic, create_single_topic, update_single_topic, delete_single_topic, notes, single_note, create_single_note, preflight_handler])
+        .mount("/", routes![topics, single_topic, create_single_topic, update_single_topic, delete_single_topic, notes, single_note, create_single_note, update_single_note, delete_single_note, preflight_handler])
         .attach(cors::CorsFairing)
         .attach(TigumPgConn::fairing())
         .launch();
