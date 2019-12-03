f30165d517c10da0969bcf1456985287ef6a45d6
bbsmithy <bsmith@cartrawler.com>
2019-12-03 20:48:32 +0000

    Code model

--- src/db/models/resources/code.rs
+++ src/db/models/resources/code.rs
@@ -1,26 +1,25 @@
+
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
 
-// Used when creating a new Video
-#[derive(Serialize, Deserialize)]
-pub struct NewVideo {
+// Used when creating a new ArticleSnippet
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NewCode {
+    pub content: String,
+    pub language: String,
+    pub origin: String,
     pub topic_id: i32,
     pub user_id: i32,
-    pub title: String,
-    pub iframe: String,
-    pub origin: String,
-    pub thumbnail_img: String,
 }
 
-// Used when reading or updating a Video
-#[derive(Serialize, Deserialize)]
-pub struct Video {
+// Used when reading or updating a ArtcileSnippet
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Code {
     pub id: i32,
+    pub content: String,
+    pub date_created: NaiveDateTime,
+    pub language: String,
+    pub origin: String,
     pub topic_id: i32,
     pub user_id: i32,
-    pub title: String,
-    pub iframe: String,
-    pub origin: String,
-    pub date_created: NaiveDateTime,
-    pub thumbnail_img: String,
 }
--- src/db/models/resources/mod.rs
+++ src/db/models/resources/mod.rs
@@ -3,6 +3,7 @@ pub mod note;
 pub mod video;
 pub mod image;
 pub mod documents;
+pub mod code;
 
 #[derive(Debug)]
 pub enum ResourceType {
