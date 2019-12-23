9aa3e50dbed0fe1be025fcd3d3f5164690a7803f
bbsmithy <bsmith@cartrawler.com>
2019-12-23 16:53:50 +0000

    Password verify function working

--- src/util/auth.rs
+++ src/util/auth.rs
@@ -1,9 +1,9 @@
-use bcrypt::{hash, verify, hash_with_result};
+use bcrypt::{hash, hash_with_result, verify};
 
 pub fn hash_password(plain: &String) -> Result<String, bcrypt::BcryptError> {
     Ok(hash(plain, 10)?)
 }
 
 pub fn verify_password(plain: &String, hash_string: &str) -> Result<bool, bcrypt::BcryptError> {
-   verify(plain, hash_string)
+    Ok(verify(plain, hash_string)?)
 }
