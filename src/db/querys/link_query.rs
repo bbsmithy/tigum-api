b317a26bd12271296906071a794977068bc55ba8
bbsmithy <briansmith.work578@gmail.com>
2020-08-22 16:22:13 +0100

    Link queries using ApiResponse

--- src/db/querys/link_query.rs
+++ src/db/querys/link_query.rs
@@ -6,6 +6,7 @@ use rocket::http::Status;
 use crate::db::models::resources::ResourceType;
 use crate::db::querys::TigumPgConn;
 use crate::db::querys::topic_query::remove_from_topic_resource_list;
+use crate::db::querys::topic_query::add_to_topic_resource_list;
 use crate::db::api_response::ApiResponse;
 use crate::db::models::resources::link::{Link, NewLink};
 use crate::db::models::Ids;
@@ -53,45 +54,80 @@ pub fn delete_link(conn: &TigumPgConn, id: i32, user_id: i32) -> ApiResponse {
     }
 }
 
-pub fn update_link(conn: &TigumPgConn, id: i32, link: Json<NewLink>, user_id: i32) -> Json<Link> {
-    let updated_rows = conn
+pub fn update_link(conn: &TigumPgConn, id: i32, link: Json<NewLink>, user_id: i32) -> ApiResponse {
+    let update = conn
         .query(
             "UPDATE links SET topic_id = $2, user_id = $3, title = $4 WHERE id = $1 AND user_id = $5 RETURNING *",
             &[&id, &link.topic_id, &user_id, &link.title, &user_id],
-        )
-        .unwrap();
-
-    let link_response = row_to_link(updated_rows.get(0));
-
-    Json(link_response)
+        );
+    match update {
+        Ok(rows) => {
+            let updated_row = rows.get(0);
+            let updated_link = row_to_link(updated_row);
+            ApiResponse {
+                json: json!(updated_link),
+                status: Status::raw(200)
+            }
+        },
+        Err(_err) => {
+            ApiResponse {
+                json: json!({ "error": format!("Failed to update link with id: {}", id) }),
+                status: Status::raw(500)
+            }
+        }
+    }
 }
 
-pub fn get_links(conn: &TigumPgConn, ids: Json<Ids>, user_id: i32) -> Json<Vec<Link>> {
+pub fn get_links(conn: &TigumPgConn, ids: Json<Ids>, user_id: i32) -> ApiResponse {
     let query_result = conn.query(
             "SELECT * FROM links WHERE id = ANY($1) AND user_id = $2",
             &[&ids.ids, &user_id],
-        )
-        .unwrap();
-    let mut results: Vec<Link> = vec![];
-    for row in query_result.iter() {
-        let link_response = row_to_link(row);
-        results.push(link_response);
+        );
+    match query_result {
+        Ok(rows) => {
+            let mut results: Vec<Link> = vec![];
+            for row in rows.iter() {
+                let link_response = row_to_link(row);
+                results.push(link_response);
+            }
+            ApiResponse {
+                json: json!(results),
+                status: Status::raw(200)
+            }
+        },
+        Err(_err) => {
+            ApiResponse {
+                json: json!({ "error": format!("Could not fetch links with ids: {:?}", ids) }),
+                status: Status::raw(500)
+            }
+        }
     }
-    Json(results)
 }
 
-pub fn get_link(conn: &TigumPgConn, id: i32, user_id: i32) -> Json<Link> {
+pub fn get_link(conn: &TigumPgConn, id: i32, user_id: i32) -> ApiResponse {
     let query_result = conn
         .query(
             "SELECT * FROM links WHERE id = $1 AND user_id = $2",
             &[&id, &user_id],
-        )
-        .unwrap();
-    let link_response = row_to_link(query_result.get(0));
-    Json(link_response)
+        );
+    match query_result {
+        Ok(rows) => {
+            let link_response = row_to_link(rows.get(0));
+            ApiResponse {
+                json: json!(link_response),
+                status: Status::raw(200)
+            }
+        },
+        Err(_err) => {
+            ApiResponse {
+                json: json!({ "error": format!("Failed to get link with id: {}", id) }),
+                status: Status::raw(500)
+            }
+        }
+    }
 }
 
-pub fn create_link(conn: &TigumPgConn, link: &Json<NewLink>, user_id: i32) -> Result<Link, Error> {
+pub fn create_link(conn: &TigumPgConn, link: &Json<NewLink>, user_id: i32) -> ApiResponse {
     let query_result = conn.query(
             "INSERT INTO links (title, topic_id, user_id, source) VALUES ($1, $2, $3, $4) RETURNING *",
             &[
@@ -104,8 +140,26 @@ pub fn create_link(conn: &TigumPgConn, link: &Json<NewLink>, user_id: i32) -> Re
     match query_result {
         Ok(new_link_rows) => {
             let new_row = new_link_rows.get(0);
-            Ok(row_to_link(new_row))
+            let new_link = row_to_link(new_row);
+            let query_result = add_to_topic_resource_list(
+                &conn,
+                link.topic_id,
+                new_link.id,
+                ResourceType::Link,
+            );
+            match query_result {
+                Ok(_rows_updated) => ApiResponse { json: json!(new_link), status: Status::raw(200) },
+                Err(_error) => ApiResponse {
+                    json: json!({ "error": format!("Could not create snippet {}", new_link.topic_id )}),
+                    status: Status::raw(500)
+                }
+            }
         }
-        Err(error) => Err(error)
+        Err(_err) => ApiResponse {
+            json: json!({
+                "error": format!("Could not create link {}", link.topic_id )
+            }),
+            status: Status::raw(500)
+        }
     }
 }
