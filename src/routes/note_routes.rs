2edbf49c5ecb2a27591f3b2de8120caf67d7bee3
bbsmithy <briansmith.work578@gmail.com>
2020-08-23 15:26:18 +0100

    Notes using ApiResponse

--- src/routes/note_routes.rs
+++ src/routes/note_routes.rs
@@ -27,40 +27,22 @@ fn delete_single_note(conn: TigumPgConn, note_id: i32, auth_user: User) -> ApiRe
 }
 
 #[put("/notes/<note_id>", format = "application/json", data = "<note>")]
-fn update_single_note(conn: TigumPgConn, note_id: i32, note: Json<Note>, auth_user: User) -> Json<Note> {
+fn update_single_note(conn: TigumPgConn, note_id: i32, note: Json<Note>, auth_user: User) -> ApiResponse {
     update_note(&conn, note_id, note, auth_user.id)
 }
 
 #[post("/notes/create-note", format = "application/json", data = "<note>")]
 fn create_single_note(conn: TigumPgConn, note: Json<NewNote>, auth_user: User) -> ApiResponse {
-    let new_note_query_result = create_note(&conn, &note, auth_user.id);
-    match new_note_query_result {
-        Ok(new_note) => {
-            let query_result = add_to_topic_resource_list(&conn, note.topic_id, new_note.id, ResourceType::Note);
-            match query_result {
-                Ok(_rows_updated) => ApiResponse { json: json!(new_note), status: Status::raw(200) },
-                Err(_error) => ApiResponse {
-                    json: json!({ "error": format!("Could not create note {}", new_note.topic_id )}),
-                    status: Status::raw(500)
-                }
-            }
-        },  
-        Err(_error) => ApiResponse {
-            json: json!({
-                "error": format!("Could not create snippet {}", note.topic_id )
-            }),
-            status: Status::raw(500)
-        }
-    }
+    create_note(&conn, &note, auth_user.id)
 }
 
 #[get("/notes/<note_id>")]
-fn single_note(conn: TigumPgConn, note_id: i32, _auth_user: User) -> Json<Note> {
+fn single_note(conn: TigumPgConn, note_id: i32, _auth_user: User) -> ApiResponse {
     get_note(&conn, note_id)
 }
 
 #[post("/notes", format = "application/json", data = "<note_ids>")]
-fn notes(conn: TigumPgConn, note_ids: Json<NoteIds>, auth_user: User) -> Json<Vec<Note>> {
+fn notes(conn: TigumPgConn, note_ids: Json<NoteIds>, auth_user: User) -> ApiResponse {
     get_notes(&conn, note_ids, auth_user.id)
 }
 
