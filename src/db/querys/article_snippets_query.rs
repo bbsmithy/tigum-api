81c3ac403f1c45dcb53d46d383f48285a2e79a62
bbsmithy <briansmith.work578@gmail.com>
2020-12-07 14:55:25 +0000

    get snippets by DESC

--- src/db/querys/article_snippets_query.rs
+++ src/db/querys/article_snippets_query.rs
@@ -81,7 +81,7 @@ pub fn update_article_snippet(
 
 pub fn get_article_snippets(conn: &TigumPgConn, ids: Json<Ids>, user_id: i32) -> ApiResponse {
     let query_result = conn.query(
-            "SELECT * FROM article_snippets WHERE id = ANY($1) AND user_id = $2 ORDER BY date_created ASC",
+            "SELECT * FROM article_snippets WHERE id = ANY($1) AND user_id = $2 ORDER BY date_created DESC",
             &[&ids.ids, &user_id],
         );
     match query_result {
