ddd6708879e7331e92e0ad924440b2e721035530
bbsmithy <briansmith.work578@gmail.com>
2021-04-05 20:24:29 +0100

    update_topic_mod API

--- src/routes/topic_routes.rs
+++ src/routes/topic_routes.rs
@@ -1,5 +1,6 @@
 use crate::db;
 use rocket::Route;
+use rocket::http::Status;
 
 //Use Macros
 use rocket_contrib::json::Json;
@@ -13,7 +14,14 @@ use db::querys::TigumPgConn;
 use db::api_response::ApiResponse;
 
 //Database Querys
-use db::querys::topic_query::{create_topic, delete_topic, get_topic, get_topics, update_topic};
+use db::querys::topic_query::{
+    create_topic,
+    delete_topic,
+    get_topic,
+    get_topics,
+    update_topic,
+    update_topic_mod_date
+};
 
 //////////////////////
 //// TOPIC ROUTES ////
@@ -31,6 +39,27 @@ async fn update_single_topic(
     topic: Json<Topic>,
 ) -> ApiResponse {
     update_topic(&conn, topic_id, topic).await
+}
+
+#[put("/topics/update-mod-date/<topic_id>", format = "application/json")]
+async fn update_mod_date(
+    conn: TigumPgConn,
+    topic_id: i32
+) -> ApiResponse {
+    match update_topic_mod_date(&conn, topic_id).await {
+        Ok(_rows) => {
+            ApiResponse {
+                json: json!({"msg": "Success"}),
+                status: Status::raw(200)
+            }
+        },
+        Err(err) => {
+            ApiResponse {
+                json: json!({"error": format!("Could not update topic mod date")}),
+                status: Status::raw(500)
+            }
+        }
+    }
 }
 
 #[post("/topics/create-topic", format = "application/json", data = "<topic>")]
@@ -52,6 +81,7 @@ pub fn get_topic_routes() -> Vec<Route> {
     routes![
         delete_single_topic,
         update_single_topic,
+        update_mod_date,
         create_single_topic,
         single_topic,
         topics
