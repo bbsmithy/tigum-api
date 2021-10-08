cb0d51a9b9b955bb0b2c9ed6588f6058a0682592
bbsmithy <briansmith.work578@gmail.com>
2021-10-09 00:51:50 +0100

    Removed wildcard from allow origin

--- src/cors.rs
+++ src/cors.rs
@@ -47,7 +47,7 @@ impl Fairing for CorsFairing {
             },
             None => println!("{}", "No Set-Cookie header in res")
         }
-        let allowed_origin_header = Header::new("Access-Control-Allow-Origin", "*");
+        let allowed_origin_header = Header::new("Access-Control-Allow-Origin", "https://tigum.io");
         // Add CORS headers to allow all origins to all outgoing requests
         res.set_header(allowed_origin_header);
         res.set_header(Header::new(
