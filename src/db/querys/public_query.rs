4ef89ebb5a55cb57e6faca92e31bdeaeaa311b3b
bbsmithy <briansmith.work578@gmail.com>
2021-10-08 22:30:55 +0100

    public links query

--- src/db/querys/public_query.rs
+++ src/db/querys/public_query.rs
@@ -1,162 +1,107 @@
-use crate::db::querys::TigumPgConn;
 use crate::db::api_response::ApiResponse;
 use rocket::http::Status;
-// use crate::db::parsing_util::{
-//     row_to_user, 
-//     parse_topic_result, 
-//     parse_note_result,
-//     parse_article_snippet_result,
-//     parse_link_result,
-//     parse_video_result
-// };
+use crate::db::models::topic::Topic;
+use crate::db::models::resources::note::Note;
+use crate::db::models::resources::video::Video;
+use crate::db::models::resources::link::Link;
+use crate::db::models::resources::article_snippets::ArticleSnippet;
+use crate::db::models::user::AuthUser;
+use rocket_contrib::databases::diesel;
+use diesel::{QueryDsl, RunQueryDsl};
+use diesel::ExpressionMethods;
 
 
-pub fn get_public_topics_for_user(conn: &TigumPgConn, user_name: String) -> ApiResponse {
-
-    ApiResponse {
-        json: json!("All good"),
-        status: Status::raw(200)
-    }
-
-    // // CHECK IF USER EXISTS
-    // let check_for_user = conn.run(move |c|
-    //     c.query(
-    //         "SELECT * FROM users WHERE name = $1",
-    //         &[&user_name]
-    //     )
-    // );
-
-    // if let Ok(result) = check_for_user {
-    //     if let Some(user) = result.get(0) {
-    //         let parsed_user = row_to_user(user);
-    //         // GET TOPICS FOR USER
-    //         let get_user_public_topics = conn.run(move |c|
-    //             c.query(
-    //                 "SELECT * FROM topics WHERE user_id = $1 AND published = true",
-    //                 &[&parsed_user.id]
-    //             )
-    //         );
-    //         if let Ok(results) = get_user_public_topics {
-    //             let parsed_topics = parse_topic_result(results);
-    //             ApiResponse {
-    //                 status: Status::raw(200),
-    //                 json: json!({ "topics": parsed_topics })
-    //             }
-    //         } else {
-    //             ApiResponse {
-    //                 status: Status::raw(500),
-    //                 json: json!({ "error": "Something went wrong" })
-    //             }
-    //         }
-    //     } else {
-    //         ApiResponse {
-    //             status: Status::raw(404),
-    //             json: json!({ "msg": "Failed to find user" })
-    //         }
-    //     }
-
-    // } else {
-    //     ApiResponse {
-    //         status: Status::raw(404),
-    //         json: json!({ "msg": "Failed to find user" })
-    //     }
-    // }
-    
+pub fn get_public_topics_for_user(conn: &diesel::PgConnection, user_name: String) -> ApiResponse {
+    use crate::schema::users::dsl::*;
+    use crate::schema::topics::dsl::*;
+    let check_for_user = users.filter(name.eq(user_name)).get_result::<AuthUser>(conn);
+    match check_for_user {
+        Ok(row) => {
+            let published_topics_res = topics.filter(user_id.eq(row.id)).filter(published.eq(true)).get_results::<Topic>(conn);
+            match published_topics_res {
+                Ok(rows) => {
+                    ApiResponse {
+                        json: json!({ "topics": rows }),
+                        status: Status::raw(200)
+                    }
+                },
+                Err(_err) => {
+                    ApiResponse {
+                        status: Status::raw(500),
+                        json: json!({ "error": "Something went wrong" })
+                    }
+                }
+            }
+        },
+        Err(_err) => {
+            ApiResponse {
+                status: Status::raw(404),
+                json: json!({ "msg": "Failed to find user" })
+            }
+        }
+    }    
 }
 
-pub fn get_public_notes_in_topic(conn: &TigumPgConn, topic_id: i32) -> ApiResponse {
-    ApiResponse {
-        json: json!("All good"),
-        status: Status::raw(200)
+pub fn get_public_notes_in_topic(conn: &diesel::PgConnection, notes_topic_id: i32) -> ApiResponse {
+    use crate::schema::notes::dsl::*;
+    let published_notes_res = notes.filter(topic_id.eq(notes_topic_id)).filter(published.eq(true)).get_results::<Note>(conn);
+    if let Ok(result) = published_notes_res {
+        ApiResponse {
+            status: Status::raw(200),
+            json: json!({ "notes": result })
+        }
+    } else {
+        ApiResponse {
+            status: Status::raw(404),
+            json: json!({ "msg": "Failed to find user" })
+        }
     }
-    // let public_notes = conn.run(move |c|
-    //     c.query(
-    //         "SELECT * FROM notes WHERE topic_id = $1 AND published = true",
-    //         &[&topic_id]
-    //     )
-    // );
-    // if let Ok(result) = public_notes {
-    //     ApiResponse {
-    //         status: Status::raw(200),
-    //         json: json!({ "notes": parse_note_result(result) })
-    //     }
-    // } else {
-    //     ApiResponse {
-    //         status: Status::raw(404),
-    //         json: json!({ "msg": "Failed to find user" })
-    //     }
-    // }
 }
 
-pub fn get_public_videos_in_topic(conn: &TigumPgConn, topic_id: i32) -> ApiResponse {
-    ApiResponse {
-        json: json!("All good"),
-        status: Status::raw(200)
+pub fn get_public_videos_in_topic(conn: &diesel::PgConnection, videos_topic_id: i32) -> ApiResponse {
+    use crate::schema::videos::dsl::*;
+    let published_videos_res = videos.filter(topic_id.eq(videos_topic_id)).filter(published.eq(true)).get_results::<Video>(conn);
+    if let Ok(result) = published_videos_res {
+        ApiResponse {
+            status: Status::raw(200),
+            json: json!({ "videos": result })
+        }
+    } else {
+        ApiResponse {
+            status: Status::raw(404),
+            json: json!({ "msg": "Failed to find user" })
+        }
     }
-    // let public_notes = conn.run(move |c|
-    //     c.query(
-    //         "SELECT * FROM videos WHERE topic_id = $1 AND published = true",
-    //         &[&topic_id]
-    //     )
-    // );
-    // if let Ok(result) = public_notes {
-    //     ApiResponse {
-    //         status: Status::raw(200),
-    //         json: json!({ "videos": parse_video_result(result) })
-    //     }
-    // } else {
-    //     ApiResponse {
-    //         status: Status::raw(404),
-    //         json: json!({ "msg": "Failed to find user" })
-    //     }
-    // }
 }
 
-pub fn get_public_snippets_in_topic(conn: &TigumPgConn, topic_id: i32) -> ApiResponse {
-    ApiResponse {
-        json: json!("All good"),
-        status: Status::raw(200)
+pub fn get_public_snippets_in_topic(conn: &diesel::PgConnection, snippet_topic_id: i32) -> ApiResponse {
+    use crate::schema::article_snippets::dsl::*;
+    let published_videos_res = article_snippets.filter(topic_id.eq(snippet_topic_id)).filter(published.eq(true)).get_results::<ArticleSnippet>(conn);
+    if let Ok(result) = published_videos_res {
+        ApiResponse {
+            status: Status::raw(200),
+            json: json!({ "snippets": result })
+        }
+    } else {
+        ApiResponse {
+            status: Status::raw(404),
+            json: json!({ "msg": "Failed to find user" })
+        }
     }
-    // let public_notes = conn.run(move |c|
-    //     c.query(
-    //         "SELECT * FROM article_snippets WHERE topic_id = $1 AND published = true",
-    //         &[&topic_id]
-    //     )
-    // );
-    // println!("{:?}", public_notes);
-    // if let Ok(result) = public_notes {
-    //     ApiResponse {
-    //         status: Status::raw(200),
-    //         json: json!({ "snippets": parse_article_snippet_result(result) })
-    //     }
-    // } else {
-    //     ApiResponse {
-    //         status: Status::raw(404),
-    //         json: json!({ "msg": "Failed to find user" })
-    //     }
-    // }
 }
 
-pub fn get_public_links_in_topic(conn: &TigumPgConn, topic_id: i32) -> ApiResponse {
-    ApiResponse {
-        json: json!("All good"),
-        status: Status::raw(200)
+pub fn get_public_links_in_topic(conn: &diesel::PgConnection, link_topic_id: i32) -> ApiResponse {
+    use crate::schema::links::dsl::*;
+    let published_videos_res = links.filter(topic_id.eq(link_topic_id)).filter(published.eq(true)).get_results::<Link>(conn);
+    if let Ok(result) = published_videos_res {
+        ApiResponse {
+            status: Status::raw(200),
+            json: json!({ "links": result })
+        }
+    } else {
+        ApiResponse {
+            status: Status::raw(404),
+            json: json!({ "msg": "Failed to find user" })
+        }
     }
-    // let public_notes = conn.run(move |c|
-    //     c.query(
-    //         "SELECT * FROM links WHERE topic_id = $1 AND published = true",
-    //         &[&topic_id]
-    //     )
-    // );
-    // if let Ok(result) = public_notes {
-    //     ApiResponse {
-    //         status: Status::raw(200),
-    //         json: json!({ "links": parse_link_result(result) })
-    //     }
-    // } else {
-    //     ApiResponse {
-    //         status: Status::raw(404),
-    //         json: json!({ "msg": "Failed to find user" })
-    //     }
-    // }
 }
