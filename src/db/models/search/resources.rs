035e91ce515cbbce83c833e25d65889f50eea2a1
bbsmithy <briansmith.work578@gmail.com>
2021-10-09 00:21:30 +0100

    Search queries using orm

--- src/db/models/search/resources.rs
+++ src/db/models/search/resources.rs
@@ -1,11 +1,19 @@
 use serde::{Deserialize, Serialize};
+use diesel::{Queryable, QueryableByName};
+use diesel::sql_types::{Integer, Text};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Queryable, QueryableByName, Serialize, Deserialize, Debug)]
 pub struct ResourceResult {
+    #[sql_type="Integer"]
     pub topic_id: i32,
+    #[sql_type="Integer"]
     pub resource_id: i32,
+    #[sql_type="Text"]
     pub result_type: String,
+    #[sql_type="Text"]
     pub title: String,
+    #[sql_type="Text"]
     pub misc: String,
+    #[sql_type="Text"]
     pub misc2: String
 }
--- src/db/querys/search_resources_query.rs
+++ src/db/querys/search_resources_query.rs
@@ -1,100 +1,74 @@
 use crate::db;
-// use rocket_contrib::databases::postgres::Row;
 use rocket::http::Status;
-
+use crate::db::api_response::ApiResponse;
+use rocket_contrib::databases::diesel;
+use diesel::{RunQueryDsl};
+use diesel::sql_query;
 use db::models::search::resources::ResourceResult;
-use db::querys::TigumPgConn;
-use db::api_response::ApiResponse;
-
-const FIND_BY_TITLE_QUERY_STRING: &str = "
-SELECT 'topic' result_type, id as topic_id, title, 0 as resource_id, 'none' as misc, 'none' as misc2, date_updated FROM topics WHERE lower(title) LIKE $1 AND user_id = $2
-UNION ALL
-SELECT 'note' result_type, topic_id, title, id as resource_id, 'none' as misc, 'none' as misc2, date_updated FROM notes WHERE lower(title) LIKE $1 AND user_id = $2
-UNION ALL
-SELECT 'video' result_type, topic_id, title, id as resource_id, iframe as misc, thumbnail_img as misc2, date_updated FROM videos
-WHERE lower(title) LIKE $1 AND user_id = $2
-UNION ALL
-SELECT 'link' result_type, topic_id, title, id as resource_id, source as misc, 'none' as misc2, date_updated FROM links
-WHERE lower(title) LIKE $1 AND user_id = $2
-UNION ALL
-SELECT 'snippet' result_type, topic_id, content as title, id as resource_id, origin as misc, title as misc2, date_updated FROM article_snippets
-WHERE lower(content) LIKE $1 AND user_id = $2
-ORDER BY date_updated DESC
-";
-
-const FIND_BY_TOPIC_ID: &str = "
-SELECT 'video' result_type, topic_id, title, id as resource_id, iframe as misc, thumbnail_img as misc2, date_updated FROM videos
-WHERE topic_id = $1 AND user_id = $2
-UNION ALL
-SELECT 'link' result_type, topic_id, title, id as resource_id, source as misc, 'none' as misc2, date_updated FROM links
-WHERE topic_id = $1 AND user_id = $2
-UNION ALL
-SELECT 'snippet' result_type, topic_id, content as title, id as resource_id, origin as misc, title as misc2, date_updated FROM article_snippets
-WHERE topic_id = $1 AND user_id = $2
-UNION ALL
-SELECT 'note' result_type, topic_id, title, id as resource_id, 'none' as misc, 'none' as misc2, date_updated FROM notes WHERE topic_id = $1 AND user_id = $2
-ORDER BY date_updated DESC
-";
 
 
-// fn row_to_resource_result(row: &Row) -> ResourceResult {
-//     let result_type = row.get(0);
-//     let base_result = ResourceResult {
-//         result_type: result_type,
-//         topic_id: row.get(1),
-//         title: row.get(2),
-//         resource_id: row.get(3),
-//         misc: row.get(4),
-//         misc2: row.get(5)
-//     };
-//     base_result
-// }
-
-pub fn find_by_title(conn: &TigumPgConn, title: String, user_id: i32) -> ApiResponse {
-    ApiResponse {
-        json: json!("All good"),
-        status: Status::raw(200)
+pub fn find_by_title(conn: &diesel::PgConnection, search_title: String, user_id: i32) -> ApiResponse {
+    let find_by_title_like_query_string = format!("
+        SELECT 'topic' result_type, id as topic_id, title, 0 as resource_id, 'none' as misc, 'none' as misc2, date_updated FROM topics WHERE lower(title) LIKE '%{q_title}%' AND user_id = {uid}
+        UNION ALL
+        SELECT 'note' result_type, topic_id, title, id as resource_id, 'none' as misc, 'none' as misc2, date_updated FROM notes WHERE lower(title) LIKE '%{q_title}%' AND user_id = {uid}
+        UNION ALL
+        SELECT 'video' result_type, topic_id, title, id as resource_id, iframe as misc, thumbnail_img as misc2, date_updated FROM videos
+        WHERE lower(title) LIKE '%{q_title}%' AND user_id = {uid}
+        UNION ALL
+        SELECT 'link' result_type, topic_id, title, id as resource_id, source as misc, 'none' as misc2, date_updated FROM links
+        WHERE lower(title) LIKE '%{q_title}%' AND user_id = {uid}
+        UNION ALL
+        SELECT 'snippet' result_type, topic_id, content as title, id as resource_id, origin as misc, title as misc2, date_updated FROM article_snippets
+        WHERE lower(content) LIKE '%{q_title}%' AND user_id = {uid}
+        ORDER BY date_updated DESC
+    ", q_title = search_title, uid = user_id);
+    let result = sql_query(find_by_title_like_query_string).get_results::<ResourceResult>(conn);
+    match result {
+        Ok(rows) => {
+            ApiResponse {
+                json: json!(rows),
+                status: Status::raw(200)
+            }
+        },
+        Err(err) => {
+            println!("{}", err);
+            ApiResponse {
+                json: json!("nope"),
+                status: Status::raw(500)
+            }
+        }
     }
-    // let like_title = format!("%{}%", title.to_lowercase());
-    // let result_query = conn.run(move |c|
-    //     c.query(FIND_BY_TITLE_QUERY_STRING, &[&like_title, &user_id])
-    // );
-    // return_search_results(result_query)
 }
 
-pub fn find_by_topic_id(conn: &TigumPgConn, topic_id: i32, user_id: i32) -> ApiResponse {
-
-    ApiResponse {
-        json: json!("All good"),
-        status: Status::raw(200)
+pub fn find_by_topic_id(conn: &diesel::PgConnection, topic_id: i32, user_id: i32) -> ApiResponse {
+    let find_by_topic_id_query = format!("
+        SELECT 'video' result_type, topic_id, title, id as resource_id, iframe as misc, thumbnail_img as misc2, date_updated FROM videos
+        WHERE topic_id = {tid} AND user_id = {uid}
+        UNION ALL
+        SELECT 'link' result_type, topic_id, title, id as resource_id, source as misc, 'none' as misc2, date_updated FROM links
+        WHERE topic_id = {tid} AND user_id = {uid}
+        UNION ALL
+        SELECT 'snippet' result_type, topic_id, content as title, id as resource_id, origin as misc, title as misc2, date_updated FROM article_snippets
+        WHERE topic_id = {tid} AND user_id = {uid}
+        UNION ALL
+        SELECT 'note' result_type, topic_id, title, id as resource_id, 'none' as misc, 'none' as misc2, date_updated FROM notes WHERE topic_id = {tid} AND user_id = {uid}
+        ORDER BY date_updated DESC
+    ", tid = topic_id, uid = user_id);
+    let result = sql_query(find_by_topic_id_query).get_results::<ResourceResult>(conn);
+    match result {
+        Ok(rows) => {
+            ApiResponse {
+                json: json!(rows),
+                status: Status::raw(200)
+            }
+        },
+        Err(err) => {
+            println!("{}", err);
+            ApiResponse {
+                json: json!("nope"),
+                status: Status::raw(500)
+            }
+        }
     }
-
-    // let result_query = conn.run(move |c|
-    //     c.query(FIND_BY_TOPIC_ID, &[&topic_id, &user_id])
-    // );
-    // return_search_results(result_query)
 }
-
-// fn return_search_results(result_query: Result<Vec<rocket_contrib::databases::postgres::Row>, rocket_contrib::databases::postgres::Error>) -> ApiResponse {
-//     match result_query {
-//         Ok(rows) => {
-//             let mut resource_results: Vec<ResourceResult> = vec![];
-//             for row in rows.iter() {
-//                 let resource_result_row = row_to_resource_result(row);
-//                 println!("{:?}", row);
-//                 resource_results.push(resource_result_row)
-//             };
-//             ApiResponse {
-//                 json: json!(resource_results),
-//                 status: Status::raw(200)
-//             }
-//         },
-//         Err(err) => {
-//             println!("query error: {:?}", err);
-//             ApiResponse {
-//                 json: json!({ "error": "Query failed" }),
-//                 status: Status::raw(500)
-//             }
-//         }
-//     }
-// }
